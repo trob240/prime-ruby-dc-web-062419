98f3448bc5c7b985a1ea64e68a4353de00991ef2
Trevahr Robertson <trevahr@yahoo.com>
2019-04-24 15:33:50 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -14,3 +14,28 @@ def prime?(n)
       
      return true
 end
+
+=begin
+require 'benchmark'
+
+
+def prime?
+  n = gets.to_i
+         if n <= 1
+         return false
+       end
+
+      for i in (2..Math.sqrt(n))
+         if (n%i) == 0
+         	return false
+         	end
+      end
+     
+      
+     return true
+end
+
+puts Benchmark.measure{prime?}
+
+prime?
+=end
