e9c938ec6ccfc2e6e07c05c4f298e5258a26545d
Trevahr Robertson <trevahr@yahoo.com>
2019-04-24 01:49:42 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
 # Add  code here!
+
+def prime?(n)
+    if n <= 1
+      false
+
+    elsif
+      n == 2 
+        true
+    
+  else
+    
+     (2..(Math.sqrt(9))
+
+    end
+    
+    
+    
+ end
