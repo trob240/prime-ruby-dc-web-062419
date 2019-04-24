cc16dbcf5fd5f8da6ed86851224cfd46cd20a75e
Trevahr Robertson <trevahr@yahoo.com>
2019-04-24 14:55:38 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,28 +1,16 @@
 # Add  code here!
 
 def prime?(n)
-    if n <= 1
-      puts false
+         if n <= 1
+         return false
+       end
 
-    elsif
-      n == 2 || n == 3
-      puts  true
-    
-  elsif
-    
-    
-   for i in (2..(Math.sqrt(n)).round)
-   
-    if n % i == 0
-   
-    puts  false
-
-  else
-    puts true
-    
-    
-    
- end
- end
- end
- end
+      for i in (2..Math.sqrt(n))
+         if (n%i) == 0
+         	return false
+         	end
+      end
+     
+      
+     return true
+end
